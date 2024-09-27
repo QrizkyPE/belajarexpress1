4b44b551de5dbc964b4f7ff503fd5a1e4554da71
Qrizky <qpeffendy@gmail.com>
2024-09-27 11:20:57 +0700

    belajar ejs

--- index.js
+++ index.js
@@ -1,22 +1,90 @@
-// const express = require("express") // import modul express
-// const app = express() // inisialisasi express
-// const port = 3000 //port
+const express = require("express") // import modul express
+const app = express() // inisialisasi express
+const port = 3000 //port
 
+// gunakan ejs
+app.set('view engine','ejs');
 
 app.get("/", (req, res)=>{
-    res.send("Hello");
+    // res.send("Hello");
+    const berita = [
+        {
+            judul:"Berita 1",
+            isi: "Isi berita 1"
+        },
+        {
+            judul: "Berita 2",
+            isi: "Isi berita 2"
+        }
+
+    ];
+    
+    res.render('index',{title: 'Halaman Home', berita});
+});
+
+app.get("/prodi", (req, res)=>{
+    
+    const prodis = [
+        
+        // Prodi : ["Sistem Informasi"],
+        // Singkatan : ["SI"],
+        // Fakultas :["FIKR"]
+        
+            // "Prodi" : "Sistem Informasi",
+            // "Fakultas"  : "FIKR",
+            // "Singkatan"  : "SI"
+        
+
+            {
+                "Prodi"     : "Sistem Informasi",
+                "Fakultas"  : "FIKR",
+                "Singkatan"  : "SI"
+            },
+            {
+                "Prodi" : "Informatika",
+                "Fakultas"  : "FIKR",
+                "Singkatan"  : "IF"
+            },
+            {
+                "Prodi" : "Teknik Elektro",
+                "Fakultas"  : "FIKR",
+                "Singkatan"  : "TE"
+            },
+            {
+                "Prodi" : "Manajemen Informatika",
+                "Fakultas"  : "FIKR",
+                "Singkatan"  : "MI"
+            },
+            {
+                "Prodi" : "Manajemen",
+                "Fakultas"  : "FEB",
+                "Singkatan"  : "MJ"
+            },
+            {
+                "Prodi" : "Akuntansi",
+                "Fakultas"  : "FEB",
+                "Singkatan"  : "AK"
+            },
+            
+        
+    
+    ]
+    
+    res.render('prodi',{title: 'Halaman prodi', prodis});
 });
 
 //get = mengambil
 app.get("/about",(req, res)=>{
     // res.send("About us");
-    res.sendFile(__dirname+"/about.html");
+    // res.sendFile(__dirname+"/about.ejs");
+    res.render('about')
 })
 //put = update
 
 app.get("/contact", (req, res)=>{
     // res.send("Contact Us");
-    res.sendFile(__dirname+"/contact.html");
+    // res.sendFile(__dirname+"/contact.ejs");
+    res.render('contact')
 });
 
 
